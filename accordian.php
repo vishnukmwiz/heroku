5892dc51781c2d344af492c4640414da03f3feda
Wiz <59883413+vishnukmwiz@users.noreply.github.com>
2021-03-10 15:47:35 +0530

    Add files via upload

--- accordian.php
+++ accordian.php
@@ -18,7 +18,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">      
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link "  href="mycompletewebpage.html">Home</a>
+                            <a class="nav-link "  href="mycompletewebpage.php">Home</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="#">Accordian</a>
